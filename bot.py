db13128ec958f7001215346a3e942b1d095d96d9
Mahendra Ribadiya <mahendratek@gmail.com>
2024-06-14 20:20:59 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username="", password="")
 
+######  upload a picture #######
+bot.upload_photo("car.jpg", caption="Cocacola drinking")
+
+######  follow someone #######
+bot.follow("ritikroshan")
+
+######  send a message #######
+bot.send_message("I am Mahendra from Vipcodder", ['user1','user2'])
+
+######  get follower info #######
+my_followers = bot.get_user_followers("mahendrakumarribadiya")
+for follower in my_followers:
+    print(follower)
+
+bot.unfollow_everyone()
